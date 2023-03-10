61a70811b83685faf15d02834daaf1a87330b0e9
Поцылуенко <83078431+EvgenPotsulyenko@users.noreply.github.com>
2023-03-10 12:18:15 +0300

    Add files via upload

---
+++ Dz3.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Dz3</title>
+</head>
+<body>
+2 + 2 = <?php echo 2 + 2; ?>
+</body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+$name = $_POST["firstname"];
+$age = $_POST["age"];
+echo "Hello my name is <b>".$name . "</b>". "<br>";
+echo "I’m <b>".$age . "</b>";
+?>
